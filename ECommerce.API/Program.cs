2cc3571cb07e27498d895b9653374118e9436355
Can Oğuzorhan <oguzorhancan1@gmail.com>
2026-07-04 16:27:34 +0300

    - Switch database provider to PostgreSQL

--- ECommerce.API/Program.cs
+++ ECommerce.API/Program.cs
@@ -51,7 +51,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
 
