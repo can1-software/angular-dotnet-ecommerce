7d67e2cbbe97fbdce5b0c17d483d0457c0160170
Can Oğuzorhan <oguzorhancan1@gmail.com>
2026-07-04 20:05:49 +0300

    - cors hatası giderildi

--- ECommerce.API/Program.cs
+++ ECommerce.API/Program.cs
@@ -84,7 +84,9 @@ builder.Services.AddCors(options =>
 
                 return false;
 
-            return uri.Host is "localhost" or "127.0.0.1";
+            return uri.Host is "localhost"
+                or "127.0.0.1"
+                or "angular-dotnet-ecommerce-client.onrender.com";
 
         })
 
